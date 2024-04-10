d043d4ecda07c941bf5835c8537a95a7211a328e
Rohan Kumar Gouda <rohan.gouda@medfin.in>
2024-04-10 14:42:53 +0530

    login admin added

--- controllers/userlistCtrl.js
+++ controllers/userlistCtrl.js
@@ -1,14 +1,10 @@
 (function () {
     app.controller("userlistCtrl", function (AclService,$scope, $location, $cookies, $cookieStore, app, api) {
-        var lgc = this;
-        lgc.email = null;
-        lgc.password = null;
-        lgc.resetemail = null;
-        lgc.otp = null;
-        lgc.token = null;
-
+        $scope.can = AclService.can;
+        $scope.userList = [];
+        $scope.keyword = '';
         app.setTitle("User List");
-
+      
         $scope.openCreateUserModal = function () {
             $scope.userId = '';
             $scope.email = '';
@@ -28,7 +24,48 @@
             $('#userModal').modal('show');
 
         }
-        
+        $scope.roleList = api.getRoleList();
+        $scope.roleList.then(function mySucces(r) {
+            if (r.data.length > 0) {
+                $scope.roleList = r.data;
+            }
+
+        });
+        $scope.search = function (keyword, startIndex) {
+
+            // if (startIndex == 0) {
+            //     $scope.currentPage = 1;
+            // }
+            var request = {
+                "keyword": keyword
+            };
+
+            $.blockUI({
+                message: 'Please wait... we are processing your request',
+                baseZ: 15000
+            });
+            var promise = api.getUsersByKeyword(request);
+            promise.then(function mySucces(r) {
+                App.unblockUI();
+                if (r.data.statusCode === 200) {
+                    $scope.userList = r.data.data;
+                    $scope.total = r.data.count;
+
+                } else {
+                    $scope.userList = [];
+                    swal("Error!", "No data found..!", "error");
+                }
+                $.unblockUI();
+            });
+            // }
+
+        }
+
+        $scope.pagination = function (a, page) {
+            var startIndex = ($scope.pageSize * (page - 1));
+            $scope.search($scope.keyword, startIndex);
+        }
+
     })
 
 })();
